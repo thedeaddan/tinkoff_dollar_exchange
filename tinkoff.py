2af0d4e9a3cb8a02069708042284f3cdccd84a72
Даниил <40400854+thedeaddan@users.noreply.github.com>
2022-02-28 18:00:20 +0300

    где-то чёто попукал уже не помню

--- tinkoff.py
+++ tinkoff.py
@@ -1,4 +1,4 @@
-import json
+limport json
 import requests
 import time
 import vk_api
@@ -8,7 +8,7 @@ import os
 
 #!!!!
 #Надо изменить на свои данные!!!
-vk = vk_api.VkApi(token = "TOKEN VK") #Токен ВК
+vk = vk_api.VkApi(token = "токен") #Токен ВК
 peer_id = 2000000014 #ID Беседы куда бот будет писать уведомления
 #!!!!
 
@@ -21,13 +21,14 @@ def vk_send(text):
 		time.sleep(10)
 		vk.method("messages.send",{"random_id":0,"message":text,"peer_id":peer_id})
 
-def vk_edit(text):
+def vk_edit(text,message_id):
 	print(text)
 	try:
-		vk.method("messages.edit",{"peer_id":peer_id,"message":text,"message_id": message_id,},)
+		vk.method("messages.edit",{"peer_id":peer_id,"message":text,"message_id": message_id})
 	except:
 		time.sleep(10)
-		vk.method("messages.edit",{"peer_id":peer_id,"message":text,"message_id": message_id,},)
+		vk.method("messages.send",{"random_id":0,"message":text,"peer_id":peer_id})
+
 
 @timeout(10)
 def get_response():
@@ -36,7 +37,6 @@ def get_response():
 def get_dollar_exchange():
 	try:
 		response = get_response()
-		status = response.status_code
 		jsons = json.loads(response.text)
 		buy = jsons["payload"]["rates"][1]["buy"]
 		sell = jsons["payload"]["rates"][1]["sell"]
@@ -56,12 +56,14 @@ def send_message_in_zeros(buy):
 	message_id = message.get("items")[0].get("id")
 	message_text = message.get("items")[0].get("text")
 	if "Курс $ на данный момент:" in message_text:
-		vk_edit("Курс $ на данный момент: "+str(buy)+" руб.")
+		vk_edit("Курс $ на данный момент: "+str(buy)+" руб.",message_id)
 	else:
 		vk_send("Курс $ на данный момент: "+str(buy)+" руб.")
+
 old_buy = 0
 oldest_buy = 0
 highest = 86.50	
+
 while True:
 	now = str(datetime.datetime.now())[:-7]
 	buy,sell = get_dollar_exchange()
@@ -70,17 +72,20 @@ while True:
 		second_new = str(buy).split(".")[1]
 		first_old = str(old_buy).split(".")[0]
 		second_old = str(old_buy).split(".")[1]
-		if int(first_new)> 87:
-			vk_send("Цена $ выше 87 руб. Уведомляю о изменении и копеек:\nНовая цена: "+str(buy)+" руб. \nСтарая цена: "+str(old_buy)+" руб.")
+		if int(first_new)>= 87:
+			vk_send("Цена $ выше 87 руб. Уведомляю о изменении и копеек:\nНовая цена: "+str(buy)+" руб.\nСтарая цена: "+str(old_buy)+" руб.")
 		if int(first_new) > int(first_old):
 			minus_rub = str(int(first_new) - int(first_old))
 			minus_kop = str(int(second_new) - int(second_old))
 			if buy > highest:
-				vk_send("Внимание!\n@all")
-				vk_send("Цена выше локальной вершины!\nНовая цена: "+str(buy)+" руб.\n Старая цена: "+str(old_buy)+" руб.")
+				for i in range(0,4):
+					vk_send("Внимание!\n@all")
+					time.sleep(0.5)
+					vk_send("Цена выше локальной вершины!\nНовая цена: "+str(buy)+" руб.\nСтарая цена: "+str(old_buy)+" руб.")
+					time.sleep(0.5)
 				highest = buy
 			else:
-				vk_send("Цена $ выросла на "+minus_rub+" руб.\nНовая цена: "+str(buy)+" руб.\n Старая цена: "+str(old_buy)+" руб.") 
+				vk_send("Цена $ выросла на "+minus_rub+" руб.\nНовая цена: "+str(buy)+" руб.\nСтарая цена: "+str(old_buy)+" руб.") 
 			for i in range(0,7):
 				get_response()
 			time.sleep(10)
